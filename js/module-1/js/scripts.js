548aa8d379884359e724db1d21e3981dcefd83fb
Antonkriukov <antonkriukov87@gmail.com>
2018-06-25 12:22:24 +0300

    js module-1

---
+++ js/module-1/js/scripts.js
@@ -0,0 +1,24 @@
+'use strict'
+
+//1 задание
+const ADMIN_LOGIN = prompt('Admin Login');
+
+if (ADMIN_LOGIN == 'admin') {
+    
+const ADMIN_PASSWORD = prompt( 'Admin Password');
+
+if (ADMIN_PASSWORD =='m4ngo1zh4ackz0r') {
+  alert ('Добро пожаловать!');
+
+} else if ( ADMIN_PASSWORD == null) {
+  alert('Отменено пользователем!');  
+} else {
+  alert('Доступ запрещен!');
+}
+
+} else if ( ADMIN_LOGIN == null) {
+    alert('Отменено пользователем!');
+} else {
+    alert ('Доступ запрещен!');
+}
+
