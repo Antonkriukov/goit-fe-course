a58753d16e77ee8dd77843e91f19e929e34c0491
Antonkriukov <antonkriukov87@gmail.com>
2018-09-09 10:37:36 +0300

    module-2

--- js/module-2/js/scripts.js
+++ js/module-2/js/scripts.js
@@ -1,19 +1,17 @@
 'use strict';
 const numbers = [];
 let total = 0;
-let userInput ;
-
+let userInput;
+//Число введенное пользователем записывается в массив чисел
 do {
   userInput = Number(prompt('Введите число'));
-  (numbers.push(parseFloat(userInput))) ;
-  for (let value of numbers) {
-    total += value;}
-} while (userInput !== 0  ) 
-
-  if (numbers.length > 1){
-    alert(`Общая сумма чисел равна ${total}`)};
-
-
-
-
-
+  numbers.push(parseFloat(userInput));
+} while (userInput !== 0);
+// сложить общую сумму всех элементов массива
+for (let value of numbers) {
+  total += value;
+}
+// По окончанию ввода, если массив не пустой, вывести alert с текстом `Общая сумма чисел равна ${сумма}
+if (numbers.length > 1) {
+  alert(`Общая сумма чисел равна ${total}`);
+}
