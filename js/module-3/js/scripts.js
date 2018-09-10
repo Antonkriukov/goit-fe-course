012984ed968068fe26a9ca62d55646a3f13dca58
Antonkriukov <antonkriukov87@gmail.com>
2018-09-10 22:40:53 +0300

    module-3

--- js/module-3/js/scripts.js
+++ js/module-3/js/scripts.js
@@ -1,46 +1,49 @@
-'use strict'
+'use strict';
 
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+// Взаимодействия с пользователем
+const enterLogin = 'Please, enter your login';
+const loginError = 'Ошибка! Логин должен быть от 4 до 16 символов';
+const noLogin = 'Данный логин отсутствует в базе логинов';
+const pushLogin = 'Логин успешно добавлен!';
+const useLogin = 'Такой логин уже используется!';
+// Спрашиваем логин у пользователя
 
-console.log(logins);
+let login = prompt(enterLogin);
+// Проверка на длину от 4 до 16-ти символов включительно
+const isLoginValid = login => login.length >= 4 && login.length <= 16;
 
-
-let login = prompt('Please, enter your login');
-
-const checkLoginValidity = login => login.length >= 4 && login.length <= 16;
-       
-
-const checkLoginValidityResult = checkLoginValidity(login);
-
-
-const checkIfLoginExists = function(ls, l) {
-    return logins.includes(login)
-    
+const checkLoginValidityResult = isLoginValid(login);
+// проверяет наличие login в массиве allLogins, возвращая true
+const isLoginUnique = function(ls, l) {
+  return logins.includes(login);
 };
 
-
-const checkIfLoginExistsResult = checkIfLoginExists(logins, login);
-
-
+const checkIfLoginExistsResult = isLoginUnique(logins, login);
+/* Функцию addLogin(logins, login) которая:
+    - Принимает новый логин и массив всех логинов как параметры
+    - Проверяет валидность логина используя вспомогательную функцию isLoginValid
+    - Если логин не валиден, прекратить исполнение функции addLogin 
+      и вернуть строку 'Ошибка! Логин должен быть от 4 до 16 символов'
+    - Если логин валиден, функция addLogin проверяеть уникальность логина 
+      с помощью функции isLoginUnique
+    - Если isLoginUnique вернет true, addLogin добавляет новый логин 
+       в logins и возвращает строку 'Логин успешно добавлен!'
+    - Если isLoginUnique вернет false, тогда addLogin не добавляет 
+       логин в массив и возвращает строку 'Такой логин уже используется!'
+   */
 const addLogin = function(logins, login) {
-    console.log('Entered login: ', login);
-    console.log('login length:', login.length);
-    console.log('Length Validity (4-16):', checkLoginValidityResult);
-
-    if (!checkLoginValidityResult ) {
-        return console.log('Ошибка! Логин должен быть от 4 до 16 символов');
-    
-    }
-    if (!checkIfLoginExistsResult) {
-        console.log('Данный логин отсутствует в базе логинов');
-        console.log('Логин успешно добавлен!');
-        logins.push(login);
-        
-    } else {
-        console.log('Такой логин уже используется!');
-    }
+  if (!checkLoginValidityResult) {
+    return console.log(loginError);
+  }
+  if (!checkIfLoginExistsResult) {
+    console.log(noLogin);
+    console.log(pushLogin);
+    logins.push(login);
+  } else {
+    console.log(useLogin);
+  }
 };
 
 addLogin(logins, login);
-console.log('Добавления логина в массив:' ,logins);
-
+console.log('Добавления логина в массив:', logins);
