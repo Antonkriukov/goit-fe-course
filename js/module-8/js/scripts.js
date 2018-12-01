a57acbf6f2f5096766aec2352f56bd40e818b850
Antonkriukov <antonkriukov87@gmail.com>
2018-12-01 21:06:16 +0200

    module-8

--- js/module-8/js/scripts.js
+++ js/module-8/js/scripts.js
@@ -1,76 +1,112 @@
 'use strict';
 
 const galleryItems = [
-  { preview: 'img/preview-1.jpeg', fullview: 'img/fullview-1.jpeg', alt: "alt text 1" },
-  { preview: 'img/preview-2.jpeg', fullview: 'img/fullview-2.jpeg', alt: "alt text 2" },
-  { preview: 'img/preview-3.jpeg', fullview: 'img/fullview-3.jpeg', alt: "alt text 3" },
-  { preview: 'img/preview-4.jpeg', fullview: 'img/fullview-4.jpeg', alt: "alt text 4" },
-  { preview: 'img/preview-5.jpeg', fullview: 'img/fullview-5.jpeg', alt: "alt text 5" },
-  { preview: 'img/preview-6.jpeg', fullview: 'img/fullview-6.jpeg', alt: "alt text 6" },
+  {
+    preview: 'img/preview-1.jpeg',
+    fullview: 'img/fullview-1.jpeg',
+    alt: 'alt text 1',
+  },
+  {
+    preview: 'img/preview-2.jpeg',
+    fullview: 'img/fullview-2.jpeg',
+    alt: 'alt text 2',
+  },
+  {
+    preview: 'img/preview-3.jpeg',
+    fullview: 'img/fullview-3.jpeg',
+    alt: 'alt text 3',
+  },
+  {
+    preview: 'img/preview-4.jpeg',
+    fullview: 'img/fullview-4.jpeg',
+    alt: 'alt text 4',
+  },
+  {
+    preview: 'img/preview-5.jpeg',
+    fullview: 'img/fullview-5.jpeg',
+    alt: 'alt text 5',
+  },
+  {
+    preview: 'img/preview-6.jpeg',
+    fullview: 'img/fullview-6.jpeg',
+    alt: 'alt text 6',
+  },
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
   const imgPreviewArr = document.querySelectorAll('img');
   imgPreviewArr[1].classList.add('focused');
-  imgFullview.setAttribute('src', galleryItems[0].fullview);
 });
 
 const imageGallery = document.querySelector('.js-image-gallery');
 
-const container = document.createElement('div');
-const fullView = document.createElement('div');
-const imgFullview = document.createElement('img');
-const preView = document.createElement('ul');
-
-fullView.classList.add('fullview');
-preView.classList.add('preview');
-
-const result = createCards (galleryItems);
-imageGallery.append(...result); 
-
-function createCards (galleryItems) {
-  return galleryItems.map(item => createPostCard(item));
-};
+// create fullview div
+const fullviewDiv = document.createElement('div');
+fullviewDiv.classList.add('fullview');
 
 
-function createPostCard ({preview, fullview, alt}) {  
+const previewDiv = document.createElement('ul');
+previewDiv.classList.add('preview');
 
-  const previewItem = document.createElement('li');
-  const imgPreview = document.createElement('img');
+const imgsLi = createImages(galleryItems);
 
 
-  imgFullview.setAttribute('alt', alt);
-  imgPreview.setAttribute('src', preview);
-  imgPreview.setAttribute('alt', alt);
-  imgPreview.setAttribute('data-fullview', fullview);
 
-  fullView.appendChild(imgFullview);
-  preView.append(previewItem);
-  previewItem.appendChild(imgPreview);
+previewDiv.append(...imgsLi);
 
-  container.append(fullView, preView);
+imageGallery.append(fullviewDiv, previewDiv);
 
-  return container;
+function createImages(galleryItems) {
+  return galleryItems.map((item, index) => {
+      if (!index) {
+          createFullView(item);
+      }
+
+      return createImageItem(item);
+  });
 }
 
-preView.addEventListener('click', getImg);
-function getImg (event) {
+function createImageItem({preview, fullview, alt}) {
+  const li = document.createElement('li');
+  const img = document.createElement('img');
+
+  img.setAttribute('src', preview);
+  img.setAttribute('alt', alt);
+  img.setAttribute('data-fullview', fullview);
+
+  li.appendChild(img);
+  return li;
+}
+
+function createFullView({fullview, alt}) {
+  const img = document.createElement('img');
+
+  img.setAttribute('src', fullview);
+  img.setAttribute('alt', alt);
+
+  fullviewDiv.appendChild(img);
+}
+
+
+previewDiv.addEventListener('click', getImg);
+
+function getImg(event) {
   const target = event.target;
   const name = target.nodeName
   if (name !== 'IMG') {
-  return;
-}
+      return;
+  }
   getThatImg(target);
 }
 
-function getThatImg (pointedImg) {
+function getThatImg(pointedImg) {
   const source = pointedImg.getAttribute('data-fullview');
-  const previousImg = preView.querySelector('li img.focused');
+  const previousImg = previewDiv.querySelector('li img.focused');
 
   if (previousImg) {
       previousImg.classList.remove('focused');
   }
 
   pointedImg.classList.add('focused');
-  imgFullview.setAttribute('src', source);
+  fullviewDiv.children[0].setAttribute('src', source);
 }
